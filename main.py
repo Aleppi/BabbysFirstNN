2d67987ee8d30aacddc1d70f2440d0fece46b151
Aleppi <hectortib@gmail.com>
2019-04-08 14:01:13 +0200

    Began adding training labels

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import mnist as mn
 import neuralnetwork as nn
 
 data = mn.train_images()/255
+labels = [1 for i in range(len(mn.train_labels()))]
+
+print(labels[0])
 
 layer_sizes = (784,5,5,10)
-
 
 NN = nn.NeuralNetwork(layer_sizes)
 print(NN.predict(data[0]), '\n')
