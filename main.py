aed00e3704d6eb2d85035f112088554433e5c5c9
Aleppi <hectortib@gmail.com>
2019-04-23 17:24:27 +0200

    Added stochastic gradient descent method

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ data = mn.train_images()/255
 data.shape = (len(mn.train_images()),-1,1)
 labels = [labelArrCreator(i) for i in mn.train_labels()]
 
-layer_sizes = (784,10,16,10)
+layer_sizes = (784,5,10)
 
-#training_data = ( for x,y in zip(data, labels))
+training_data = list(zip(data[:10],labels[:10]))
 
-#mini_batch = (data[:10].reshape(784,1),labels[:10].reshape(10,1))
+mini_batch = list(zip(data[:10],labels[:10]))
 
 NN = nn.NeuralNetwork(layer_sizes)
+NN.SGD(training_data, 5, 10, 3)
+print("Prediction: {} \n Answer: {}".format(NN.forward(data[0]), labels[0]))
 #print(training_data)
 #NN.update_mini_batch(mini_batch, 3)
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -64,9 +64,20 @@ class NeuralNetwork:
                     nabla_w[l][j,k] = self.partialWeight(x, y, l+1, j, k)
         return (nabla_b, nabla_w)
     
-   # def update_mini_batch(self, mini_batch, eta):
-   #     nabla_b = [np.zeros(b.shape) for b in self.biases]
-   #     nabla_w = [np.zeros(w.shape) for w in self.weights]
-   #     for x,y in mini_batch:
-   #         delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-   #         print(type(delta_nabla_w))
+    def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x,y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w - (eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta):
+        for i in range(epochs):
+            np.random.shuffle(training_data)
+            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0,len(training_data),mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+
